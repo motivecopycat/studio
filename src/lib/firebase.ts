0d60e7660650f3dabf065a564812b9b7523050ce
Copycat Motive <motivecopycat@gmail.com>
2025-10-28 16:31:10 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCHaNnkWhDHPo4Rrm_7cEjlO7G1zhuLPxU',
-  authDomain: 'kikasite-251f4.firebaseapp.com',
-  projectId: 'kikasite-251f4',
-  storageBucket: 'kikasite-251f4.appspot.com',
-  messagingSenderId: '1061899209288',
-  appId: '1:1061899209288:web:8630fb47b7b3ded399667d',
-  measurementId: 'G-5FR0E8HTDG',
+  apiKey: "AIzaSyCHaNnkWhDHPo4Rrm_7cEjlO7G1zhuLPxU",
+  authDomain: "kikasite-251f4.firebaseapp.com",
+  projectId: "kikasite-251f4",
+  storageBucket: "kikasite-251f4.appspot.com",
+  messagingSenderId: "1061899209288",
+  appId: "1:1061899209288:web:8630fb47b7b3ded399667d",
+  measurementId: "G-5FR0E8HTDG",
 };
 
 // Initialize Firebase
