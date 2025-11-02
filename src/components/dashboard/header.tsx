1284bedef7bdc90d3c1325b788837215541ad3e5
Copycat Motive <motivecopycat@gmail.com>
2025-11-02 14:12:08 +0000

    Remove the profile in header section but show the profile in side naviga

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -105,9 +105,6 @@ export default function DashboardHeader() {
       <div className="flex items-center gap-2">
         <LanguageSwitcher />
         <ThemeSwitcher />
-        <div className="hidden sm:block">
-            <UserNav />
-        </div>
       </div>
     </header>
   );
