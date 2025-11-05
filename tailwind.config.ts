fea61b62dc2242b15a889e8994e4b685eca174e9
Copycat Motive <motivecopycat@gmail.com>
2025-11-05 05:08:56 +0000

    But this design all are not responsive to the chrome browser example lin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -14,6 +15,13 @@ export default {
       screens: {
         "2xl": "1400px",
       },
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
     extend: {
       fontFamily: {
